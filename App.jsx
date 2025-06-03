8e58427b8ded6f4517d1f021471e494920604f3d
= <coderking201@gmail.com>
2025-06-03 14:17:33 +0530

    Lesson 18 Completed Class Completed

--- App.jsx
+++ App.jsx
@@ -2,32 +2,31 @@ import React, {useState} from 'react';
 import {Button, Text, View, StyleSheet, TextInput, FlatList,ScrollView} from 'react-native';
 import exStyles from './style';
 import UserData from './components/UserData';
-
+import { SectionList } from 'react-native';
 function App() {
  const users = [
   {
     id:1,
     name:'Anil',
-    email:'abc@test.com'
+    data : ['PHP','Angular','React js']
   },
-  {
-    id:2,
-    name:'Sam',
-    email:'abc@test.com'
-  },
-  {
-    id:3,
+  {id:2,
     name:'Peter',
-    email:'abc@test.com'
-  },
+    data : ['Java','Js','HTML']},
+  {id:1,
+    name:'Anil',
+    data : ['C','C++','Python']},
+  {id:1,
+    name:'Anil',
+    data : ['CSS','BootStrap','HTML']}
  ]
   return (
     <>
     <Text style={{fontSize:31}}>
-      Component in Loop with FlatList
+      Section List in React-Native
     </Text>
-    <FlatList data={ users } renderItem={( { item } )=>
-     <UserData item={ item }/> }/>
+   <SectionList sections={users} renderItem={({item})=><Text style={{fontSize:20,marginLeft:20}} >{item}</Text>}
+   renderSectionHeader={({section:{name}})=><Text style={{color:'red',fontSize:25}}>{name}</Text>}/>
     </>
   );
 }
