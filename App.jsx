82dd2cff6fe55f8a8f6dd478beb5c46441e5dfbe
= <coderking201@gmail.com>
2025-08-16 22:30:40 +0530

    Lesson 32 Modal_Dialog Box in react native completed

--- App.jsx
+++ App.jsx
@@ -1,36 +1,52 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Button, TouchableOpacity, ActivityIndicator} from 'react-native';
+import {View, Text, Button,Modal, TouchableOpacity, ActivityIndicator} from 'react-native';
 import {StyleSheet, TouchableHighlight} from 'react-native';
 import style from './style';
 
 const App = () => {
-  const [show, setShow] = useState(false);
-  const [selectedRadio, setSelectedRadio]= useState(1)
-
-  const displayLoader = ()=>{
-    setShow(true)
-    setTimeout(()=>{
-      setShow(false)
-    },3000)
-    ///commit
-  }
+const [showModal,setShowModal] = useState(false)
   return (
     <View style={styles.main}>
-        {/* <ActivityIndicator size={200} color={'blue'}/> */}
-        {/* <ActivityIndicator size={'small'} color={'red'}/> */}
-        {show?<ActivityIndicator size={'small'} color={'red'}/>:null}
-        <ActivityIndicator size={100} color={'green'} animating={show}/>
-        <Button title='Show Loader' onPress={displayLoader}/>
+      <Modal transparent={true} visible={showModal} animationType='slide'>
+        <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>
+                  Hello Code Step by Step
+              </Text>
+              <Button title='CLOSE MODAL'  onPress={()=>setShowModal(false)}/>
+            </View>
+        </View>
+      </Modal>
+      <View style={styles.buttonView}>
+        <Button title="Open Modal" onPress={()=>setShowModal(true)}/>
+        </View>
     </View>
   );
-};
-
-export default App;
-const styles = StyleSheet.create({
-  main: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+}
+ const styles = StyleSheet.create({
+  main:{
+    flex:1
   },
-  
-});
+  buttonView:{
+    flex:1,
+    justifyContent:'flex-end'
+  },
+  centeredView:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  modalView:{
+backgroundColor:'#ffffff',
+padding:30,
+borderRadius:20,
+shadowColor:'black',
+elevation:5
+  },
+  modalText:{
+    fontSize:30,
+    marginBottom:20,
+  }
+
+ })
+export default App;
