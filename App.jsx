6a5468a5a2d609c63f3ef1cb4bb690110d752b14
= <coderking201@gmail.com>
2025-08-17 12:43:00 +0530

    Lesson 33 Pressable | Long press

--- App.jsx
+++ App.jsx
@@ -1,52 +1,37 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Button,Modal, TouchableOpacity, ActivityIndicator} from 'react-native';
-import {StyleSheet, TouchableHighlight} from 'react-native';
+import {View, Text, Button,StyleSheet,Pressable} from 'react-native';
 import style from './style';
 
 const App = () => {
-const [showModal,setShowModal] = useState(false)
+
   return (
-    <View style={styles.main}>
-      <Modal transparent={true} visible={showModal} animationType='slide'>
-        <View style={styles.centeredView}>
-            <View style={styles.modalView}>
-              <Text style={styles.modalText}>
-                  Hello Code Step by Step
-              </Text>
-              <Button title='CLOSE MODAL'  onPress={()=>setShowModal(false)}/>
-            </View>
-        </View>
-      </Modal>
-      <View style={styles.buttonView}>
-        <Button title="Open Modal" onPress={()=>setShowModal(true)}/>
-        </View>
+    <View style={styles.main} >
+      <Pressable
+        onPress={()=>{console.warn("normal on press")}}
+        onLongPress={()=>{console.warn("long press")}}
+        onPressIn={()=>console.warn("press in")}
+        onPressOut={()=>console.warn("press out")}
+        >
+        <Text style={styles.pressableBtn}> Pressable</Text>
+      </Pressable>
     </View>
   );
 }
  const styles = StyleSheet.create({
-  main:{
-    flex:1
-  },
-  buttonView:{
-    flex:1,
-    justifyContent:'flex-end'
-  },
-  centeredView:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
-  },
-  modalView:{
-backgroundColor:'#ffffff',
-padding:30,
-borderRadius:20,
-shadowColor:'black',
-elevation:5
-  },
-  modalText:{
-    fontSize:30,
-    marginBottom:20,
-  }
-
+main:{
+  flex:1,
+  justifyContent:'center'
+},
+pressableBtn:{
+  backgroundColor:'blue',
+  color:'white',
+  padding:10,
+  margin:10,
+  borderRadius:10,
+  fontSize:20,
+  textAlign:'center',
+  shadowColor:'#000',
+  elevation:5,
+}
  })
 export default App;
