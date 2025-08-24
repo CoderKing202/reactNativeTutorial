6e260cb469ad6fd452daa13e11d1462dc201f592
= <coderking201@gmail.com>
2025-08-24 08:24:47 +0530

    Lesson 36 How to install npm package

--- App.jsx
+++ App.jsx
@@ -1,22 +1,27 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Button,StyleSheet, StatusBar} from 'react-native';
+import {View, Text, Button,StyleSheet, StatusBar, Platform} from 'react-native';
 import style from './style';
 
 const App = () => {
 const [hide,setHide] = useState(false)
 const [barStyle,setBarStyle] = useState("default")
   return (
-    <View style={styles.container} >
-      <StatusBar backgroundColor={'red'} barStyle={barStyle} hidden={hide}/>
-      <Button title='Toggle Status Bar' onPress={()=>{setHide(!hide)}}/>
-      <Button title='Update Style' onPress={()=>{setBarStyle("dark-content")}}/>
+    <View  >
+     <Text>Platform : {Platform.OS}</Text> 
+     {
+      Platform.OS == 'android'?
+      <View style={{height:100,width:100, backgroundColor:'green'}}></View>
+      :<View style={{height:100,width:100, backgroundColor:'red'}}></View>
+     }
+     <Text style={styles.text}>Hello</Text>
+     <Text>{JSON.stringify(Platform.constants.reactNativeVersion.minor)}</Text>
     </View>
   );
 }
  const styles = StyleSheet.create({
-container:{
-  flex:1,
-  justifyContent:'center',
+text:{
+  color:Platform.OS?'orange':'blue',
+  fontSize:20
 }
  })
 export default App;
