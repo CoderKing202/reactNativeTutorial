7bf140fcd696e4d154cfd2df1b1d0a9c3ab548dc
= <coderking201@gmail.com>
2025-08-26 18:14:48 +0530

    Lesson37_Make custom modal dialog box

--- App.jsx
+++ App.jsx
@@ -1,11 +1,42 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Button,StyleSheet, StatusBar, Platform} from 'react-native';
+import {View, Text, Button,StyleSheet} from 'react-native';
 import style from './style';
-import WebView from 'react-native-webview';
+
 
 const App = () => {
-  return (
-<WebView source={{uri:'https://www.google.com'}}/>//
+  const [show,setShow] = useState(false)
+return (
+<View style={styles.container}>
+  { show?<View style={styles.modal}>
+    <View style={styles.body}>
+    <Text>Some Text</Text>
+    <Button title="Close" onPress={()=>setShow(false)}/>
+    </View>
+  </View>:null
+  }
+  <Button title="OPEN DIALOG" onPress={()=>setShow(true)}/>
+</View>
   );
 }
 export default App
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'flex-end',
+},
+modal:{
+  flex:1,
+  backgroundColor:'rgba(50,50,50,0.5)',
+  justifyContent:'center',
+  alignItems:'center',
+},
+body:{
+  backgroundColor:'white',
+  height:300,
+  width:300,
+  padding:20,
+  justifyContent:'flex-end',
+  borderRadius:10
+}
+})
