dd1f17759cc3435e69d5ae253853578092d0e65f
= <coderking201@gmail.com>
2025-08-26 15:02:28 +0530

    Lesson 37_How to install npm package

--- App.jsx
+++ App.jsx
@@ -1,28 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import {View, Text, Button,StyleSheet, StatusBar, Platform} from 'react-native';
 import style from './style';
+import WebView from 'react-native-webview';
 
 const App = () => {
-const [hide,setHide] = useState(false)
-const [barStyle,setBarStyle] = useState("default")
   return (
-    <View  >
-     <Text>Platform : {Platform.OS}</Text> 
-     {
-      Platform.OS == 'android'?
-      <View style={{height:100,width:100, backgroundColor:'green'}}></View>
-      :<View style={{height:100,width:100, backgroundColor:'red'}}></View>
-     }
-     <Text style={styles.text}>Hello</Text>
-     <Text>{JSON.stringify(Platform.constants.reactNativeVersion.minor)}</Text>
-    </View>
+<WebView source={{uri:'https://www.google.com'}}/>
   );
 }
- const styles = StyleSheet.create({
-text:{
-  color:Platform.OS?'orange':'blue',
-  fontSize:20
-}
- })
-export default App;
-//test
+export default App
