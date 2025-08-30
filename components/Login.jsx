1403061ec23e9cf9c20a04df6b84269ac3294605
= <coderking201@gmail.com>
2025-08-30 15:00:59 +0530

    Lesson 44 Tab Navigation

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,9 +1,16 @@
-import { View,Text,Button} from "react-native"
+import { useState } from "react"
+import { View,Text,Button, TextInput} from "react-native"
 export const Login = (props)=>{
+  const [name,setName] = useState("")
+   const age = 29
     return(
       <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
         <Text style={{fontSize:30}}>Login Screen</Text>
-        <Button title='Go to Home Page' onPress={()=>{props.navigation.navigate("Home")}}></Button>
+        <TextInput placeholder="Enter name" onChangeText={(text)=>setName(text)}
+          style={{fontSize:20,borderColor:'#ccc',borderWidth:2}}/>
+        <Button title='Go to Home Page' onPress={()=>{props.navigation.navigate("Home",{
+            name,
+            age})}}></Button>
       </View>
     )
   }
