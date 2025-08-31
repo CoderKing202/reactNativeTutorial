9abdb9beaf922d05f1c98c6b50e38166a00c71e0
= <coderking201@gmail.com>
2025-09-01 02:11:59 +0530

    Lesson 47 List with API data_Get List with API

--- App.jsx
+++ App.jsx
@@ -1,29 +1,28 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Button,StyleSheet, TextInput} from 'react-native';
+import {View, Text, Button,StyleSheet, TextInput ,ScrollView} from 'react-native';
 
 
 const App = () => {
-  const [data,setData] = useState(undefined)
+  const [data,setData] = useState([])
   const getApiData=async ()=>{
-    const url = "https://jsonplaceholder.typicode.com/posts/1"
+    const url = "https://jsonplaceholder.typicode.com/posts"
     let result =await fetch(url)
     result = await result.json()
     setData(result)
-    console.warn(result)
+    
   }
   useEffect(()=>{getApiData()},[])
 return (
-<View>
-  <Text style={{fontSize:30}}>API Call</Text>
-{
-  data?<View>
-    <Text>{data.id}</Text>
-    <Text>{data.userId}</Text>
-    <Text>{data.title}</Text>
-    <Text>{data.body}</Text>
-  </View>:null
-}
-</View>
+<ScrollView>
+  <Text style={{ fontSize:30 }}>List With API Call</Text>
+  {data.length?data.map((item)=><View style={{padding:10 , borderBottomColor:'#ccc'
+    , borderBottomWidth:1
+  }}>
+    <Text style={{ fontSize: 20 , backgroundColor:'#ddd'}}> Id : {item.id}</Text>
+    <Text style={{ fontSize: 20 }}> Title : {item.title}</Text>
+    <Text style={{ fontSize: 20 }}> Body : {item.body}</Text>
+  </View>):null}
+</ScrollView>
   )
 }
 export default App
