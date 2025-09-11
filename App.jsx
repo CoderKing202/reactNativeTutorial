33437ccf94fe0285d0999c0970ed15e9c44be6c0
= <coderking201@gmail.com>
2025-09-11 14:11:43 +0530

    Lesson_53_Post_API_with_input_fields_and_form_data

--- App.jsx
+++ App.jsx
@@ -1,28 +1,44 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Button,StyleSheet} from 'react-native';
+import {View, Text, Button,StyleSheet, TextInput} from 'react-native';
 
 
 const App = () => {
-  
-  const saveApiData=async ()=>{
-    const data = {name:"Sam",
-      age:34,
-      email:"sam@test.com"
-    }
+  const [name,setName] = useState('')
+  const [age,setAge] = useState(0)
+  const [email,setEmail] = useState('')
+  const saveData=async ()=>{
+
     const url = "http://10.0.2.2:3000/users"
     let result = await fetch(url,{
       method:'post',
       headers:{"Content-Type":"application/json"},
-      body:JSON.stringify(data)
+      body:JSON.stringify({name,age,email})
     })
     result = await result.json()
     console.warn(result)   
   }
 return (
   <View>
-  <Text style={{fontSize: 30}}>Post API Call </Text>
-  <Button title='Save Data' onPress={saveApiData}/>
+  <Text style={{fontSize:30}}> Post API with Input Field data </Text>
+  <TextInput style={styles.input} placeholder='Enter name' value={name}
+  onChangeText={(text)=>{setName(text)}}
+  />
+  <TextInput style={styles.input} placeholder='Enter age' value={age}
+  onChangeText={(text)=>{setAge(text)}}
+  />
+  <TextInput style={styles.input} placeholder='Enter email' value={email}
+  onChangeText={(text)=>{setEmail(text)}}
+  />
+  <Button title='Save Data' onPress={saveData}/>
 </View>
   )
 }
+const styles = StyleSheet.create({
+  input:{
+    borderColor:'skyblue',
+    borderWidth:1,
+    margin:20,
+    fontSize:20
+  }
+})
 export default App
