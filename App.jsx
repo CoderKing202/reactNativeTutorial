af141479d8eee5be12f80a66d48213f8342093b6
= <coderking201@gmail.com>
2025-09-12 15:42:33 +0530

    Lesson_54_Simple_Form_Validations

--- App.jsx
+++ App.jsx
@@ -6,8 +6,30 @@ const App = () => {
   const [name,setName] = useState('')
   const [age,setAge] = useState(0)
   const [email,setEmail] = useState('')
+  const [nameError, setNameError] = useState(false)
+  const [ageError, setAgeError] = useState(false)
+  const [emailError, setEmailError] = useState(false)
   const saveData=async ()=>{
-
+    if(!name){
+      setNameError(true)
+    }else{
+      setNameError(false)
+    }
+    if(!age){
+      setAgeError(true)
+    }else{
+      setAgeError(false)
+    }
+    if(!email){
+      setEmailError(true)
+    }
+    else{
+      setEmailError(false)
+    }
+    if(!name || !age || !email)
+    {
+      return false
+    }
     const url = "http://10.0.2.2:3000/users"
     let result = await fetch(url,{
       method:'post',
@@ -23,12 +45,15 @@ return (
   <TextInput style={styles.input} placeholder='Enter name' value={name}
   onChangeText={(text)=>{setName(text)}}
   />
+  {nameError?<Text style={styles.errorText}>Please enter Valid Name</Text>:null}
   <TextInput style={styles.input} placeholder='Enter age' value={age}
   onChangeText={(text)=>{setAge(text)}}
   />
+  {ageError?<Text style={styles.errorText}>Please enter Valid Age</Text>:null}
   <TextInput style={styles.input} placeholder='Enter email' value={email}
   onChangeText={(text)=>{setEmail(text)}}
   />
+  {emailError?<Text style={styles.errorText}>Please enter Valid Email</Text>:null}
   <Button title='Save Data' onPress={saveData}/>
 </View>
   )
@@ -38,7 +63,12 @@ const styles = StyleSheet.create({
     borderColor:'skyblue',
     borderWidth:1,
     margin:20,
-    fontSize:20
+    fontSize:20,
+    marginBottom:5,
+  },
+  errorText:{
+    color:'red',
+    marginLeft:20
   }
 })
 export default App
