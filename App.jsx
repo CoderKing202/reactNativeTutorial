941a1920a8855d9ac2be6593d72230a70d7ad2cd
= <coderking201@gmail.com>
2025-09-13 17:11:34 +0530

    Lesson 55 List with API Method

--- App.jsx
+++ App.jsx
@@ -1,74 +1,67 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Button,StyleSheet, TextInput} from 'react-native';
-
+import {View, Text, Button, StyleSheet, TextInput} from 'react-native';
 
 const App = () => {
-  const [name,setName] = useState('')
-  const [age,setAge] = useState(0)
-  const [email,setEmail] = useState('')
-  const [nameError, setNameError] = useState(false)
-  const [ageError, setAgeError] = useState(false)
-  const [emailError, setEmailError] = useState(false)
-  const saveData=async ()=>{
-    if(!name){
-      setNameError(true)
-    }else{
-      setNameError(false)
+  const [data, setData] = useState([]);
+  const getAPIData = async () => {
+    const url = 'http://10.0.2.2:3000/users';
+    let result = await fetch(url);
+    result = await result.json();
+    console.warn(result);
+    if (result) {
+      setData(result);
     }
-    if(!age){
-      setAgeError(true)
-    }else{
-      setAgeError(false)
-    }
-    if(!email){
-      setEmailError(true)
-    }
-    else{
-      setEmailError(false)
-    }
-    if(!name || !age || !email)
-    {
-      return false
-    }
-    const url = "http://10.0.2.2:3000/users"
-    let result = await fetch(url,{
-      method:'post',
-      headers:{"Content-Type":"application/json"},
-      body:JSON.stringify({name,age,email})
-    })
-    result = await result.json()
-    console.warn(result)   
-  }
-return (
-  <View>
-  <Text style={{fontSize:30}}> Post API with Input Field data </Text>
-  <TextInput style={styles.input} placeholder='Enter name' value={name}
-  onChangeText={(text)=>{setName(text)}}
-  />
-  {nameError?<Text style={styles.errorText}>Please enter Valid Name</Text>:null}
-  <TextInput style={styles.input} placeholder='Enter age' value={age}
-  onChangeText={(text)=>{setAge(text)}}
-  />
-  {ageError?<Text style={styles.errorText}>Please enter Valid Age</Text>:null}
-  <TextInput style={styles.input} placeholder='Enter email' value={email}
-  onChangeText={(text)=>{setEmail(text)}}
-  />
-  {emailError?<Text style={styles.errorText}>Please enter Valid Email</Text>:null}
-  <Button title='Save Data' onPress={saveData}/>
-</View>
-  )
-}
+  };
+  useEffect(() => {
+    getAPIData();
+  },[]);
+  return (
+    <View style={styles.container}>
+       <View style={styles.dataWrapper}>
+                <View style={{flex:1.3}}>
+                  <Text style={styles.headingStyle}>Name</Text>
+                </View>
+                <View style={{flex:2}}>
+                  <Text style={styles.headingStyle}>Age</Text>
+                </View>
+                <View style={{flex:2}}>
+                 <Text style={styles.headingStyle}>Operations</Text>
+                </View>
+                
+              </View>
+      {data.length
+        ? data.map(item => {
+            return (
+              <View style={styles.dataWrapper}>
+                <View style={{flex:1}}>
+                  <Text style={styles.itemsTextStyle}>{item.name}</Text>
+                </View>
+                <View style={{flex:1}}>
+                  <Text style={styles.itemsTextStyle}>{item.age}</Text>
+                </View>
+                <View style={{flex:1}}>
+                  <Button title='Update'/>
+                </View>
+                <View style={{flex:1}}>
+                  <Button title='Delete'/>
+                </View>
+              </View>
+            );
+          })
+        : null}
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-  input:{
-    borderColor:'skyblue',
-    borderWidth:1,
-    margin:20,
-    fontSize:20,
-    marginBottom:5,
+  container: {flex: 1},
+  dataWrapper: {
+    backgroundColor:'orange',
+    flexDirection:'row',
+    justifyContent:'space-around',
+    margin:5,
+    padding:8
   },
-  errorText:{
-    color:'red',
-    marginLeft:20
-  }
-})
-export default App
+  headingStyle:{fontWeight:'bold'},
+  itemsTextStyle:{color:'white'}
+});
+export default App;
