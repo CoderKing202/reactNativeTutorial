ef491a41f278d83c8c763cbd06bcc8771d82205f
= <coderking201@gmail.com>
2025-09-14 12:50:57 +0530

    Lesson_57_Pass_Data_to_Modal

--- App.jsx
+++ App.jsx
@@ -1,9 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Button, StyleSheet, TextInput} from 'react-native';
+import {View, Text, Button, StyleSheet, TextInput, Modal} from 'react-native';
 
 const App = () => {
   const [data, setData] = useState([]);
+  const [showModal,setShowModal] = useState(false)
+  const [selectedUser, setSelectedUser] = useState(undefined)
   const getAPIData = async () => {
+    
     const url = 'http://10.0.2.2:3000/users';
     let result = await fetch(url);
     result = await result.json();
@@ -21,6 +24,10 @@ const App = () => {
   useEffect(() => {
     getAPIData();
   },[]);
+  const updateUser= (data)=>{
+    setShowModal(true)
+    setSelectedUser(data)
+  }
   return (
     <View style={styles.container}>
        <View style={styles.dataWrapper}>
@@ -46,7 +53,7 @@ const App = () => {
                   <Text style={styles.itemsTextStyle}>{item.age}</Text>
                 </View>
                 <View style={{flex:1}}>
-                  <Button title='Update'/>
+                  <Button title='Update' onPress={()=>{updateUser(item)}}/>
                 </View>
                 <View style={{flex:1}}>
                   <Button title='Delete' onPress={()=>deleteUser(item.id)}/>
@@ -55,9 +62,21 @@ const App = () => {
             );
           })
         : null}
+        <Modal transparent={true} visible={showModal}>
+         <UserModal setShowModal={setShowModal} selectedUser={selectedUser}/>
+        </Modal>
     </View>
   );
 };
+const UserModal = (props)=>{
+  console.warn(props.selectedUser)
+  return(<View style={styles.centeredView}>
+    <View style={styles.modalView}>
+      <Text>{props.selectedUser.name}</Text>
+      <Button title='CLOSE' onPress={()=>{props.setShowModal(false)}}/>
+    </View>
+  </View>)
+}
 const styles = StyleSheet.create({
   container: {flex: 1},
   dataWrapper: {
@@ -68,6 +87,20 @@ const styles = StyleSheet.create({
     padding:8
   },
   headingStyle:{fontWeight:'bold'},
-  itemsTextStyle:{color:'white'}
+  itemsTextStyle:{color:'white'},
+  centeredView:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  modalView:{
+    backgroundColor:'white',
+    padding:60,
+    borderRadius:10,
+    shadowColor:'#000',
+    shadowOpacity:0.70,
+    elevation:5
+  }
+
 });
 export default App;
