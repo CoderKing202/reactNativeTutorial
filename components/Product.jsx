bb1225b3ed02a8117eda2fad672037f479b188f0
= <coderking201@gmail.com>
2025-09-17 15:15:06 +0530

    Lesson 5 Make Action

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,8 +1,16 @@
 import React, {useEffect, useState} from 'react';
 import {View, Text, Button, StyleSheet, TextInput, Image} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { addToCart } from './redux/action';
+import {useDispatch} from 'react-redux'
+
 const Product = (props) => {
 const item = props.item
+const dispatch = useDispatch()
+const handleaddToCart = (item)=>{
+dispatch(addToCart(item))
+
+}
   return (
     <View style={{alignItems:'center', borderBottomColor:'orange',
     borderBottomWidth:1,padding:10}}>
@@ -10,7 +18,7 @@ const item = props.item
     <Text style={{fontSize:24}}>{item.price}</Text>
     <Text style={{fontSize:24}}>{item.color}</Text>
     <Image style={{width:200,height:200}} source={{uri:item.image}}/>
-    <Button title="Add to Cart"/>
+    <Button title="Add to Cart" onPress={()=>handleaddToCart(item)}/>
   </View>
   ); 
 };
