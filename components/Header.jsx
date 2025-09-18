33cb6636cd92f396f04091485c07deb8e3016408
= <coderking201@gmail.com>
2025-09-18 17:13:53 +0530

    Lesson 8 Get Data from Redux Store

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,32 @@
 import React, {useEffect, useState} from 'react';
 import {View, Text, Button, StyleSheet, TextInput} from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import {useSelector} from 'react-redux';
+
 const Header = () => {
-  
+  const cartData = useSelector(state => state.reducer);
+  const [cartitems, setCartItems] = useState(0);
+  console.warn(cartData);
+  useEffect(() => {
+    setCartItems(cartData.length);
+  }, [cartData]);
   return (
     <View>
-      <Text style={{fontSize:30,textAlign:'right',
-        padding:10,backgroundColor:'orange'}}>0</Text>
+      
+      <Text
+        style={{
+          fontSize: 30,
+          textAlign: 'right',
+          padding: 10,
+          backgroundColor: 'orange',
+        }}>
+          <View style={{backgroundColor:'yellow', borderRadius:15,
+            height:40,width:40}}>
+        <Text style={{fontSize:30,textAlign:'center'}}>{cartitems}</Text>
+        </View>
+      </Text>
     </View>
-    
-  ); 
+  );
 };
 
-const styles = StyleSheet.create({
- 
-})
+const styles = StyleSheet.create({});
 export default Header;
- 
